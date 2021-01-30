157d3dc5cbdbfc710ff4c342c653b77ab2d16b3b
XuhaoWan <73831094+XuhaoWan@users.noreply.github.com>
2021-01-30 10:44:00 +0800

    Add files via upload

---
+++ Visilization/pearson.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#author: xhwan
+
+import numpy as np
+from sklearn.ensemble import GradientBoostingRegressor as GBR
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from scipy.stats import pearsonr
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class plot_pearson(object):
+    def __init__(self, optimal_model_name,corelation):
+        pearson_r2 = []
+        for i in range(corelation.shape[1]):
+            pearson_r1 = []
+            for j in range(corelation.shape[1]):
+                r, _ = pearsonr(corelation[:][i], corelation[:][j])
+                pearson_r1.append(r)
+            pearson_r2.append(pearson_r1)
+
+        ax1 = sns.heatmap(pearson_r2, vmin=-1, vmax=1, cmap='RdBu')
+        plt.title(optimal_model_name)
+        plt.savefig('Pcdac_pearson.jpg')
+        plt.show()
+
+
